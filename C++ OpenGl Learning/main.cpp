0bd837ea46855bc8e2c3643da3dc23c86e5d8e94
Gold(Lars H M) <lars.h.m@hotmail.com>
2020-05-11 10:11:49 +0200

    Applied texture to rendered quad

--- C++ OpenGl Learning/main.cpp
+++ C++ OpenGl Learning/main.cpp
@@ -6,15 +6,15 @@ Vertex vertices[] =
 	glm::vec3(-0.5f, 0.5f, 0.f),	glm::vec3(1.f,0.f,0.f),		glm::vec2(0.f, 1.f),
 	glm::vec3(-0.5f, -0.5f, 0.f),	glm::vec3(0.f,1.f,0.f),		glm::vec2(0.f, 0.f),
 	glm::vec3(0.5f, -0.5f, 0.f),    glm::vec3(0.f,0.f,1.f),		glm::vec2(1.f, 0.f),
-	glm::vec3(0.5f, 0.5f, 0.f),		glm::vec3(1.f,1.f,0.f),		glm::vec2(0.f, 0.f)
+	glm::vec3(0.5f, 0.5f, 0.f),		glm::vec3(1.f,1.f,0.f),		glm::vec2(1.f, 1.f)
 };
 
 unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 
 GLuint indices[] =
 {
-	0, 1, 2,	
-	0,2,3		
+	0, 1, 2,	//Triangle 1
+	0, 2, 3		//Triangle 2
 };
 
 unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
@@ -220,6 +220,34 @@ int main()
 	//BIND VAO 0
 	glBindVertexArray(0);
 
+	//TEXTURE INIT
+	int image_width = 0;
+	int image_height = 0;
+	unsigned char* image = SOIL_load_image("Images/Dagger.png", &image_width, &image_height, NULL, SOIL_LOAD_RGBA);
+
+	GLuint texture0;
+	glGenTextures(1, &texture0);
+	glBindTexture(GL_TEXTURE_2D, texture0);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	if (image)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "ERROR::TEXTURE_LOADING_FAILED" << "\n";
+	}
+
+	glActiveTexture(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	SOIL_free_image_data(image);
+
 	//MAIN LOOP
 	while (!glfwWindowShouldClose(window))
 	{
@@ -237,6 +265,13 @@ int main()
 		//Use a program
 		glUseProgram(core_program);
 
+		//Update uniforms
+		glUniform1i(glGetUniformLocation(core_program, "texture0"), 0);
+
+		//Active texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture0);
+
 		//Bind vertex array object
 		glBindVertexArray(VAO);
 
@@ -247,11 +282,17 @@ int main()
 		//End Draw
 		glfwSwapBuffers(window);
 		glFlush();
+
+		glBindVertexArray(0);
+		glUseProgram(0);
+		glActiveTexture(0);
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
 	//END OF PROGRAM
 	glfwDestroyWindow(window);
 	glfwTerminate();
+
 
 
 	return 0;
